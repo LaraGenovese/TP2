08b7736668c6f8f2d391bdcd99d9157450b22170
LaraGenovese <93852155+LaraGenovese@users.noreply.github.com>
2022-04-25 14:25:21 -0300

    Initial commit

---
+++ TP-02-main/Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñaCorrecta;
+    string contraseñaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelMsj;
+
+    void Start()
+    {
+        contraseñaCorrecta = "hola";
+        cartelMsj.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void validarContraseña()
+    {
+        contraseñaUsuario = ingresoUsuario.text;
+
+        if(contraseñaCorrecta == contraseñaUsuario)
+        {
+            cartelMsj.SetActive(true);
+            textoMsj.color = Color.blue;
+            textoMsj.text = "Bienvenid@!";
+            Debug.Log("Bienvenid@!");
+        }
+        else
+        {
+            textoMsj.text = "Contraseña INCORRECTA";
+            cartelMsj.SetActive(true);
+            Debug.Log("Constraseña incorrecta");
+        }
+    }
+}
